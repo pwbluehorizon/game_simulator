9c8f4f1cab7b565ce8db752a7c1ee23d24f60c3e
Przemek Tea <pwbluehorizon@gmail.com>
2017-06-21 21:26:29 +0200

    Tic tac toe board.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -8,5 +9,10 @@ def index():
     return 'Hello World!'
 
 
+@app.route('/game')
+def game_view():
+    return render_template('game.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
